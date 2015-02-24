626c1f9f6587d340a0dcbd103bd53ebf06f6693d
tophat8855 <hrfarley@gmail.com>
2015-02-24 09:54:01 -0800

    some rearranging

--- ruby/lib/high_school.rb
+++ ruby/lib/high_school.rb
@@ -1,27 +1,35 @@
 class HighSchool
   attr_accessor :student_count, :vacancies, :students, :dropped_out, :alumni
 
-  def initialize(student_count)
-    @student_count = student_count
-    @vacancies = student_count
+  def initialize(capacity)
+    @capacity = capacity
+    @student_count = 0
+    @vacancies = capacity
     @students = []
     @dropped_out = []
     @alumni = []
   end
 
   def enroll(student_name)
-    @vacancies -= 1;
-    @students.push(student_name)
+    if @student_count < @capacity
+      @vacancies -= 1;
+      @student_count += 1;
+      @students.push(student_name)
+    else
+      puts "High school at capacity"
+    end
   end
 
   def drop_out(student_name)
     @vacancies += 1;
+    @student_count -= 1;
     @dropped_out.push(student_name)
     @students.delete(student_name)
   end
 
   def graduate(student_name)
     @vacancies += 1;
+    @student_count -= 1;
     @students.delete(student_name)
     @alumni.push(student_name)
   end
